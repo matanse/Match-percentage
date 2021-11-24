0d981a2e021a936b7428caa68bbfe068ffd416e2
matan <matan.selig@gmail.com>
2021-11-24 14:22:30 +0100

    get the name change in input

--- src/components/MatchNumber.tsx
+++ src/components/MatchNumber.tsx
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
 
 const MatchNumber: React.FC = () => {
+  const [name1, setName1] = useState("name 1");
+  const [name2, setName2] = useState("name 2");
+
+  const changeHandler = (e: any) => {
+    if (e.target.id === "name1") {
+      setName1(e.target.value);
+    } else if (e.target.id === "name2") {
+      setName2(e.target.value);
+    }
+    console.log(name1);
+    console.log(name2);
+  };
+
   const getRandomNumber = () => {
     const number = Math.round(Math.random() * 100);
     return number;
@@ -8,8 +21,24 @@ const MatchNumber: React.FC = () => {
   return (
     <div>
       <div className="nameInput">
-        <input placeholder="name" type="text" />
-        <input placeholder="second name" type="text" />
+        <input
+          onChange={(e) => changeHandler(e)}
+          id="name1"
+          placeholder="name"
+          type="text"
+        />
+        <input
+          onChange={(e) => {
+            changeHandler(e);
+          }}
+          id="name2"
+          placeholder="second name"
+          type="text"
+        />
+      </div>
+      <div className="displayNames">
+        <h1>{name1}</h1>
+        <h1>{name2}</h1>
       </div>
       <div className="number">
         <h1>{getRandomNumber()}%</h1>
