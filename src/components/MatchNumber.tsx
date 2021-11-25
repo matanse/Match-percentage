1038db92673f77e138434f6e4e38bec7e49be809
mat <matan.selig.ubiqum@gmail.com>
2021-11-25 12:01:40 +0100

    change order of text + remove console.log

--- src/components/MatchNumber.tsx
+++ src/components/MatchNumber.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 
 const MatchNumber: React.FC = () => {
-  const [name1, setName1] = useState("name 1");
-  const [name2, setName2] = useState("name 2");
+  const [name1, setName1] = useState(String);
+  const [name2, setName2] = useState(String);
 
   const changeHandler = (e: any) => {
     if (e.target.id === "name1") {
@@ -10,14 +10,13 @@ const MatchNumber: React.FC = () => {
     } else if (e.target.id === "name2") {
       setName2(e.target.value);
     }
-    console.log(name1);
-    console.log(name2);
   };
 
   const getRandomNumber = () => {
     const number = Math.round(Math.random() * 100);
     return number;
   };
+
   return (
     <div>
       <div className="nameInput">
@@ -41,10 +40,10 @@ const MatchNumber: React.FC = () => {
       </div>
       <div className="displayNames">
         <h1>{name1}</h1>
+        <div className="number">
+          <h1>{getRandomNumber()}%</h1>
+        </div>
         <h1>{name2}</h1>
-      </div>
-      <div className="number">
-        <h1>{getRandomNumber()}%</h1>
       </div>
     </div>
   );
